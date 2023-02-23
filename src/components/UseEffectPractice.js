56d6691cc741bc1d18f5f8fdc646418ad285e7bc
srosenberg74 <srosenberg@alphaworks.tech>
2023-02-22 18:18:51 -0800

    feat(demo): useEffect demo

--- src/components/UseEffectPractice.js
+++ src/components/UseEffectPractice.js
@@ -5,12 +5,12 @@ function UseEffectPractice() {
   const [count2, setCount2] = useState(0);
 
   useEffect(() => {
-    console.log("Rerendered Decremented dependency,", count2);
-  }, [count2]);
+    console.log("count1: ", count1);
+  }, [count1]);
 
   useEffect(() => {
-    console.log("Rerender Incremented dependency,", count1);
-  }, [count1]);
+    console.log("count2: ", count2);
+  }, [count2]);
 
   useEffect(() => {
     console.log("On Initial Mount");
@@ -25,14 +25,14 @@ function UseEffectPractice() {
     setCount1(count1 + 1);
   }
 
-  function decrementCounter2() {
-    setCount2(count2 - 1);
+  function incrementCounter2() {
+    setCount2(count2 + 1);
   }
   return (
     <div>
       <h1>Practicing UseEffect</h1>
       <button onClick={incrementCounter1}>Increment Counter1</button>
-      <button onClick={decrementCounter2}>Decrement Counter2</button>
+      <button onClick={incrementCounter2}>Increment Counter2</button>
     </div>
   );
 }
